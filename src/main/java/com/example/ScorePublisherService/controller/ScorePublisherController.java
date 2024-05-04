a3ec2e7c28a3495557832cedec2cf53fc82beedd
Ritika Purswani <ritika.purswani@paytm.com>
2024-05-04 20:19:30 +0530

    commits

--- src/main/java/com/example/ScorePublisherService/controller/ScorePublisherController.java
+++ src/main/java/com/example/ScorePublisherService/controller/ScorePublisherController.java
@@ -5,43 +5,39 @@ import com.example.ScorePublisherService.producer.ScoreProducer;
 import com.example.ScorePublisherService.util.ValidationUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.SerializationException;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/score")
 public class ScorePublisherController {
 
     private static final Logger LOGGER = LogManager.getLogger(ScorePublisherController.class);
-
-    @Autowired
-    private KafkaTemplate<String, ScoreMessage> kafkaTemplate;
     @Autowired
     private ScoreProducer scoreProducer;
     @Autowired
     private ObjectMapper objectMapper;
 
-    @PostMapping("/publishScore")
+    @PostMapping("/publish")
     public ResponseEntity<String> publishScore(@RequestBody ScoreMessage scoreMessage) {
         try {
             LOGGER.info("Received score message: {}", scoreMessage);
             if (ValidationUtil.isNullOrEmpty(scoreMessage.getPlayerName()) || !ValidationUtil.isPositiveInteger(scoreMessage.getScore())) {
                 return ResponseEntity.badRequest().body("Invalid player name or score value.");
             }
+
             String message = objectMapper.writeValueAsString(scoreMessage);
 
             scoreProducer.sendMessage(message);
+
             return ResponseEntity.ok("Score published successfully to Kafka topic.");
         } catch (IllegalArgumentException e) {
             LOGGER.error("Validation failed: {}", e.getMessage());
--- src/main/java/com/example/ScorePublisherService/producer/ScoreProducer.java
+++ src/main/java/com/example/ScorePublisherService/producer/ScoreProducer.java
@@ -1,17 +1,11 @@
 package com.example.ScorePublisherService.producer;
-
-import com.example.ScorePublisherService.dto.ScoreMessage;
-import org.springframework.messaging.Message;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
 
 @Service
 public class ScoreProducer {
@@ -24,9 +18,7 @@ public class ScoreProducer {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-
         kafkaTemplate.send(topicName, message);
         LOGGER.info("Message sent successfully to Kafka topic: {}", topicName);
-
     }
 }
